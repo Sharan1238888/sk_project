144f0ad001684f33e30db4a69ae54126b1587053
unknown <Administrator@NAKSHA.TECH>
2024-05-09 18:08:06 +0530

    09-05-2024

--- src/Homepage/home.jsx
+++ src/Homepage/home.jsx
@@ -17,24 +17,42 @@ export default function Home() {
 
   return (
     <div>
-    {loggedIn ? (
-    <div className='bg'>
-      <div>
-      <Appbar />
-      {/* <Login /> */}
-     <h1 className='text'>A RELIABLE WAY TO TRAVEL</h1><br/>
-     <p className='text'>Come and listen to a story about a man named Jed a poor mountaineer barely kept his family fed so lets make the most of this beautiful day<br/>
-      since we are together it is a beautiful day in this neighborhood a beautiful day for a neighbor</p>
-  
-</div>       
-      </div>
+      {loggedIn ? (
+        <div>
+
+          <div className='bg'>
+            <div >
+              <Appbar />
+              {/* <Login /> */}
+              <h1 className='text'>A RELIABLE WAY TO TRAVEL</h1><br />
+              <p className='text'>Come and listen to a story about a man named Jed a poor mountaineer barely kept his family fed so lets make the most of this beautiful day<br />
+                since we are together it is a beautiful day in this neighborhood a beautiful day for a neighbor</p>
+
+            </div>
+          </div>
+          <div class="container-fluid no-padding choose-us">
+          <div class="section-padding"></div>
+          <div className='section-header'>
+            <h3 className='h3'>REASONS TO CHOOSE US</h3>
+          </div>
+          </div>
+
+
+
+
+
+
+
+
+        </div>
+
       ) : (
         <div>
 
-        {navigate("/Login")}
+          {navigate("/Login")}
         </div>
 
       )}
-      </div>
+    </div>
   )
 }
